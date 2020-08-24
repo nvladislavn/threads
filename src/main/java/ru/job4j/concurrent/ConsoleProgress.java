8f88de804233d26217485a765ae5ec1ee15d2a0d
Vladislav Nechaev <nvladislavn@gmail.com>
2020-08-24 12:33:03 +0500

    Прерывание нити [#343526]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,42 @@
+package ru.job4j.concurrent;
+
+/**
+ * ConsoleProgress
+ *
+ * @author Vladislav Nechaev
+ * @since 24.08.2020
+ */
+
+public class ConsoleProgress implements Runnable {
+
+    private static final int SLEEP_DURATION = 250;
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\rLoading...|");
+            try {
+                Thread.sleep(SLEEP_DURATION);
+                System.out.print("\rLoading.../");
+                Thread.sleep(SLEEP_DURATION);
+                System.out.print("\rLoading...--");
+                Thread.sleep(SLEEP_DURATION);
+                System.out.print("\rLoading...\\");
+                Thread.sleep(SLEEP_DURATION);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+}
